7f9eeab1703e0b35fd97f9bcc29da3a7ac1093d2
hyeppyday <tajokoung@gmail.com>
2024-01-05 14:27:11 +0900

    1

--- 03.플러터PJ/app_03_profile/lib/components/Profile_tab.dart
+++ 03.플러터PJ/app_03_profile/lib/components/Profile_tab.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 
 // 탑메뉴 구현 메인 클래스
 class ProfileTab extends StatefulWidget {
-    const ProfileTab({Key? key}) : super(key: key);
+  const ProfileTab({Key? key}) : super(key: key);
 
 // 기본적으로 상태관리를 해주는 메서드로 이를 생성함
 // createState() 에 실제 구현코드를 리턴하는 클래스를 연결함
@@ -16,9 +16,15 @@ class ProfileTab extends StatefulWidget {
   State<ProfileTab> createState() => _ProfileTabState();
 }
 
-// 실제로 탭메뉴 구현 코드를 리턴하는 클래스 
-class _ProfileTabState extends State<ProfileTab> 
-with SingleTickerProviderStateMixin {
+// 참고 : State<ProfileTab>
+// 추상클래스 <클래스형> -> 꺽쇠가로를 사용하는 형지정형식을
+// "제네릭" 이라 부름! 어떤 형이든지 이렇게 표현가능함
+// -> 지정된 형식의 내부 type을 지정해 주는것이 제네릭이다!
+// 예) Map<String,String> actor = {"이름":"강동원","나이":"40"};
+
+// 실제로 탭메뉴 구현 코드를 리턴하는 클래스
+class _ProfileTabState extends State<ProfileTab>
+    with SingleTickerProviderStateMixin {
   // 다중상속으로 "씽글티커프로바이더스테이트믹스인" 클래스상속
   // -> 한가지의 애니메이션을 변경시 적용해주는 클래스임!
   // -> 탭메뉴 변경시 컨텐츠를 슬라이딩 애니로 이동해서 보여줌
@@ -42,51 +48,93 @@ with SingleTickerProviderStateMixin {
 
 // 탭메뉴를 탭하여 컨텐츠를 변경할 것이므로
 // 탭컨트롤러가 필요함!!
-TabController? _tabController;
+  TabController? _tabController;
 
 // 기존 정의된 탭컨트롤러를 초기화 해야함!!!
-@override
-void initState() {
-  // 1. 부모 클래스인 State()의 초기화 메서드를 호출함!
-  super.initState();
-  // 2. 탭컨트롤러 변수에 탭컨트롤러 옵션을 설정하여 할당함!
-  _tabController = TabController(length: 2, vsync: this);
-  // length 속성 : 사용할 탭개수
-  // vsync 속성 : 탭변경시 탭메뉴와 탭뷰를 연결하는 설정
-  // -> this 키워드로 현재 탭자신을 등록함!
-} // initState 재정의 ////////////////////
+// 처음 로딩시 단 한번만 실행하여 초기화 한다!
+  @override
+  void initState() {
+    // 1. 부모 클래스인 State()의 초기화 메서드를 호출함!
+    super.initState();
+    // 2. 탭컨트롤러 변수에 탭컨트롤러 옵션을 설정하여 할당함!
+    _tabController = TabController(length: 2, vsync: this);
+    // length 속성 : 사용할 탭개수
+    // vsync 속성 : 탭변경시 탭메뉴와 탭뷰를 연결하는 설정
+    // -> this 키워드로 현재 탭자신을 등록함!
+  } // initState 재정의 ////////////////////
 
 // [1] 탭바 위젯 리턴 메서드 : _buildTabBar()
-Widget _buildTabBar(){
-  // 탭바 위젯 리턴 ////
-  return TabBar(
-    // 반드시 컨트롤러를 연결해 줘야함!!!
-    controller: _tabController,
-    tabs: [
-    // 탭바안에 탭이 2개
-    Tab(
-      icon: Icon(Icons.photo_camera),
-    ),
-    Tab(
-      icon: Icon(Icons.photo),
-    ),
-    ]
-  );
-} //////////// _buildTabBar메서드 //////////////
+  Widget _buildTabBar() {
+    // 탭바 위젯 리턴 ////
+    return TabBar(
+        // 반드시 컨트롤러를 연결해 줘야함!!!
+        controller: _tabController,
+        tabs: [
+          // 탭바안에 탭이 2개
+          Tab(
+            icon: Icon(Icons.photo_camera),
+          ),
+          Tab(
+            icon: Icon(Icons.photo),
+          ),
+        ]);
+  } //////////// _buildTabBar메서드 //////////////
+
 // [2] 탭바뷰 위젯 리턴 메서드 : _buildTabBarView()
- Widget _buildTabBarView(){
-  
-  // 탭바뷰 위젯 리턴 ////
-  return TabBarView(
-    // 반드시 컨트롤러를 연결해 줘야함!!!
-    controller: _tabController,
-    // 탭개수가 2개 이므로 2개의 탭바뷰를 구성함!
-    children: [
-    Container(color: Colors.blue,),
-    Container(color: Colors.yellow,),
-  ],
-  );
+  Widget _buildTabBarView() {
+    // 탭바뷰 위젯 리턴 ////
+    return TabBarView(
+      // 반드시 컨트롤러를 연결해 줘야함!!!
+      controller: _tabController,
+      // 탭개수가 2개 이므로 2개의 탭바뷰를 구성함!
+      children: [
+        // GridView 위젯을 사용하여 이미지를 격자로 보여준다
+        // 정해진 이미지를 가져오지 않고 웹상의 이미지정보를
+        // 가져오려면 GridView.builder를 사용해야 한다!
+        GridView.builder(
+          // 그리드 위임속성 : 연결된 이미지를 하나씩
+          // 그리드에 배치할 수 있게하는 크로스 축 카운트를 등록함
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,// 수평방향 3개 컨텐츠
+            crossAxisSpacing: 10,// 수평방향 사이간격
+            mainAxisSpacing: 10, // 수직방향 사이간격
+          ),
+          // 전체 아이템 개수 지정( 3개씩 14줄 만들예정)
+          itemCount: 42,
+          // 실제 아이템을 빌드하는 속성(중요!!!)
+          // 함수형으로 값을 셋팅함
+          // 함수 전달변수는 context는 셋팅된 모든 인트턴스 정보
+          // index는 아이템을 돌면서 카운트 되는 순번(0부터!)
+          itemBuilder: (context,index ) {
+            // debugPrint('순번: $index /컨텍스트: $context');
+            // 반드시 내부에 리턴해야함!
+            // 이미지를 네트워크로 불러오는 위젯을 사용! (dummy Image)
+            // 서비스 제공사이트  : http://picsum.photos/
+            // 요청 URL 구성 : 
+            // https://picsum.photos/id/237/200/300
+            // https://picsum.photos/id/이미지번호/가로크기/세로크기
+            // -> 이미지 번호는 1번부터 시작함(index를 활용하여 변경)
 
-} //////////// _buildTabBar메서드 //////////////
+            return Image.network("https://picsum.photos/id/${index + 1}/200/200");
+          },
+        ),
+        GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, // 수평방향 3개 컨텐츠
+            crossAxisSpacing: 10, // 수평방향 사이간격
+            mainAxisSpacing: 10, // 수직방향 사이간격
+          ),
+          itemCount: 42,
+          itemBuilder: (context,index ) {
+            // debugPrint('순번: $index /컨텍스트: $context');
+              // 위의 42번 다음 이미지부터 보이기 +43
+            return Image.network("https://picsum.photos/id/${index + 43}/200/200");
+          },
+        )
+
+        // Container(color: Colors.blue,),
+        // Container(color: Colors.yellow,),
+      ],
+    );
+  } //////////// _buildTabBar메서드 //////////////
 } //////////// _ProfileTabState 클래스 ///////////////
-
--- 03.플러터PJ/app_03_profile/lib/components/profile_drawer.dart
+++ 03.플러터PJ/app_03_profile/lib/components/profile_drawer.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 
 class ProfileDrawer extends StatelessWidget {
-  const ProfileDrawer({Key?key}):super(key:key);
+  const ProfileDrawer({Key? key}) : super(key: key);
 
 // 빌드 재정의
   @override
@@ -15,7 +15,119 @@ class ProfileDrawer extends StatelessWidget {
       // 높이값 double.infinity -> 높이값 100%로 채우기
       height: double.infinity,
       // 박스 배경색
-      color: Colors.orange.shade200,
+      color: Colors.blue,
+      // 메뉴 넣기
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            // 닫기 버튼
+            Row(
+              // 오른쪽 끝으로 정렬 : 수평방향이니까 Row에서는 mainAxis
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                // 아이콘의 버튼화! -> Inkwell 위젯
+                InkWell(
+                  // tap 이벤트시 드로어 닫기!
+                  onTap: (){
+                    // debugPrint('닫아!');
+                    // 이동클래스인 Navigator.pop()메서드 사용
+                    Navigator.pop(context);
+                    // 내용으로 context만 쓰면 현재상태로 되돌림
+                    // 열었던 드로어가 닫힌다!
+                  },
+                  child: Icon(
+                    Icons.close,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            // Home메뉴
+            Row(
+              children: [
+                Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'Home',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            // Comunity메뉴
+            Row(
+              children: [
+                Icon(
+                  Icons.people,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'Comunity',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            // Store메뉴
+            Row(
+              children: [
+                Icon(
+                  Icons.store,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'Store',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            // development메뉴
+            Row(
+              children: [
+                Icon(
+                  Icons.developer_mode,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'development',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
